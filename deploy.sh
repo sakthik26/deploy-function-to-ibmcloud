7e7b964ecfbfa596c2a3d160c0d26d8590ad7d6c
Steve Martinelli <s.martinelli@gmail.com>
2020-01-27 15:49:57 -0500

    fn testing

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 ibmcloud plugin install cloud-functions
 ibmcloud plugin list
-ibmcloud login --apikey ${IBM_CLOUD_API_KEY} > /dev/null 2>&1
-ibmcloud fn deploy --manifest manifest.yml --namespace {IBM_CLOUD_NAMESPACE}
+ibmcloud login --apikey ${IBM_CLOUD_API_KEY}
+ibmcloud fn deploy --manifest manifest.yml --namespace ${IBM_CLOUD_NAMESPACE}
 ibmcloud fn list
