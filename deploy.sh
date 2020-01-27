0d7a08ffa1fdeb8d78e1f6775e7ef6a798e1e6a5
Steve Martinelli <s.martinelli@gmail.com>
2020-01-27 15:43:43 -0500

    remove auth

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 ibmcloud plugin install cloud-functions
 ibmcloud plugin list
 ibmcloud login --apikey ${IBM_CLOUD_API_KEY} > /dev/null 2>&1
-ibmcloud fn deploy --manifest manifest.yml --auth ${IBM_CLOUD_API_KEY} --namespace {IBM_CLOUD_NAMESPACE}
+ibmcloud fn deploy --manifest manifest.yml --namespace {IBM_CLOUD_NAMESPACE}
+ibmcloud fn list
