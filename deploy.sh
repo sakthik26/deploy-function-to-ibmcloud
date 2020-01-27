42a09f13e9b38d59b805761a9e50da703a30eb23
Steve Martinelli <s.martinelli@gmail.com>
2020-01-27 15:21:21 -0500

    add fn to plugins

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ibmcloud plugin install cloud-functions
 ibmcloud plugin list
 ibmcloud login --apikey ${IBM_CLOUD_API_KEY} > /dev/null 2>&1
 ibmcloud fn deploy --manifest manifest.yml --auth ${IBM_CLOUD_API_KEY} --namespace {IBM_CLOUD_NAMESPACE}
