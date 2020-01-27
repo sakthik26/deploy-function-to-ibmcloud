aa326349e50cbca56cfe39ae6384360a131b202b
Steve Martinelli <s.martinelli@gmail.com>
2020-01-27 16:00:21 -0500

    add org and space settings

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 
 ibmcloud plugin install cloud-functions
 ibmcloud plugin list
-ibmcloud login --apikey ${IBM_CLOUD_API_KEY}
-ibmcloud fn deploy --manifest manifest.yml --namespace ${IBM_CLOUD_NAMESPACE}
+ibmcloud login --apikey ${IBM_CLOUD_API_KEY} -r ${IBM_CLOUD_REGION}
+ibmcloud target -o ${IBM_CLOUD_ORG} -s ${IBM_CLOUD_SPACE}
+ibmcloud fn deploy --manifest manifest.yml
 ibmcloud fn list
